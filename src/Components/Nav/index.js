71fe7d21d6ae547f59cf5eff590b6092e548af51
unknown <maxhd213@gmail.com>
2019-07-10 10:58:33 +0200

    working on progress.

---
+++ src/Components/Nav/index.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import './style.css';
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <ul id='nav-list'>
+                    <li id='nav-1'>
+                    <strong id='naver'>Nav 1</strong>
+                    <div id='nav-bg'>
+                        <ul>
+                            <li>
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </li>
+                    <li id='nav-2'>
+                    <strong id='naver'>Nav 2</strong>
+                    <div id='nav-bg'>
+                        <ul>
+                        <li>
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </li>
+                    <li id='nav-3'>
+                    <strong id='naver'>Nav 3</strong>
+                    <div id='nav-bg'>
+                        <ul>
+                        <li>
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </li>
+                    <li id='nav-4'>
+                    <strong id='naver'>Nav 4</strong>
+                    <div id='nav-bg'>
+                        <ul>
+                        <li>
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </li>
+                    <li id='nav-5'>
+                    <strong id='naver'>Nav 5</strong>
+                    <div id='nav-bg'>
+                        <ul>
+                        <li>
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            <a href='#'>ITEM 1</a> 
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            <a href='#'>ITEM 2</a>
+                            </li>
+                            <li>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            <a href='#'>ITEM 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Nav;
---
+++ src/Components/Top/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Top extends Component {
+    // state = {  }
+    render() {
+        return (
+            <div id='Top-con'>
+                <ul id="Top-left">
+                    <li><h1 id='Logo'>نمشي</h1></li>
+                    <li><h5>Women</h5></li>
+                    <li><h5>Men</h5></li>
+                    <li><h5>Kids</h5></li>
+                </ul>
+                <ul id="Top-right">
+                    <li><input type='text' /></li>
+                    <li><i className="fa fa-heart"></i></li>
+                    <li><i className="fa fa-user"></i></li>
+                    <li><i className="fa fa-shopping-bag"></i></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Top;
